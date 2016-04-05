6e1b8a98c84aefaa761c5d3235b1755f250bcc55
Yurii Vlasenko <vlasenkoj@gmail.com>
2016-04-06 01:02:06 +0300

    added files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#post_form').submit(function(){
-		$.post("<?= _post.php ?>", $("#post_form").serialize(),  function(response) {
-			$('#post_form').hide('slow');
-			$('#post_form_success').html(response);
-		});
-		return false;
-	});
-});
